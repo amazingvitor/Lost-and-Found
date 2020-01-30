d96f77c08a77f1d100e40aca6dd0589026dfcb74
Vitor Hugo <vitor.h8@icloud.com>
2020-01-30 16:15:53 -0300

    Initial Commit

---
+++ Lost and Found/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Lost and Found
+//
+//  Created by Vitor Hugo on 1/30/20.
+//  Copyright © 2020 Vitor Hugo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
