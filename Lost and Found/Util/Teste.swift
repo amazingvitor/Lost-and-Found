1078ff61a9779e1a7e453257bf43b499601b58e2
Vitor Hugo <vitor.h8@icloud.com>
2020-02-12 10:32:43 -0300

    teste

--- Lost and Found/Util/Teste.swift
+++ Lost and Found/Util/Teste.swift
@@ -9,31 +9,87 @@
 import SwiftUI
 
 struct Teste: View {
+    
+    var picture: String
+    var lfColor: UIColor
+    var buttonDesc: String
+    
     var body: some View {
-        VStack {
-            Text("Recent")
-                .font(.title)
-                .fontWeight(.bold)
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .padding(.horizontal)
-            ScrollView(.horizontal) {
-                HStack {
-                    ForEach(0 ..< 5) { item in
-                        CardMini()
-                            .frame(maxWidth: .infinity)
-                            .frame(width: UIScreen.main.bounds.width, height: 200)
-                            .padding(.bottom)
+        
+        HStack {
+            
+            Rectangle()
+                .frame(width: 10, height: 175)
+                .foregroundColor(.green)
+                .cornerRadius(10)
+            
+            VStack(spacing: 20) {
+                
+                // MARK: Item Pic
+                HStack( spacing: 10) {
+                    
+                    
+                    
+                    NavigationLink(destination: Text("eta son card")) {
+                        Image(self.picture)
+                            .renderingMode(.original)
+                            .resizable()
+                            .cornerRadius(10)
+                            .frame(width: 100, height: 100)
+                        
+                        
                     }
+                    
+                    VStack(alignment: .leading) {
+                        Text("Hot Wheelsabl")
+                            .font(.title)
+                            .fontWeight(.bold)
+                            .lineLimit(1)
+                        
+                        Text("50 minutes ago")
+                            .font(.caption)
+                            .padding(.bottom)
+                        
+                        
+                        Text("@AmazingVitor")
+                            .font(.subheadline)
+                    }
+                    
+                    Spacer()
+                }
+                
+                // MARK: More info button
+                NavigationLink(destination: Text("eta son")) {
+                    Text(buttonDesc)
+                        .frame(minWidth: 0, maxWidth: .infinity)
+                        .frame(height: 50)
+                        .foregroundColor(.white)
+                        .font(.system(size: 14))
+                        .background(Color(UIColor(red:0.61, green:0.15, blue:0.69, alpha:1.0)))
+                        .cornerRadius(10)
+                    
                 }
             }
-            .frame(height: 150)
         }
-        
+        .padding()
+        .cornerRadius(10)
+            // MARK: Card Format
+            .overlay(
+                
+                RoundedRectangle(cornerRadius: 10)
+                    .stroke(Color(lfColor), lineWidth: 2)
+        )
+            .padding([.bottom, .horizontal])
+            .shadow(color: Color.black.opacity(0.2), radius: 10, x: 0, y: 4)
+    }
+    
+    // TODO: More info logic
+    func logic() {
     }
 }
 
 struct Teste_Previews: PreviewProvider {
     static var previews: some View {
-        Teste()
-    }
-}
+        Teste(picture: "toy", lfColor: .secondarySystemFill, buttonDesc: "See More")
+    } // Profile_Previews
+} // PreviewProvider
